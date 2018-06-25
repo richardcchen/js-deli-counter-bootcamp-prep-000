9054d03e1100b32cb7c465f851efded0021e0571
Richard Chen <richard.c.chen@gmail.com>
2018-06-25 13:06:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function nowServing(katzDeliLine){
   if (katzDeliLine.length < 1){
     return "There is nobody waiting to be served!";
   } else{
-    return katzDeliLine.shift();
+    return "Currently serving " + katzDeliLine.shift() + ".";
   }
   
     
