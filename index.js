faf390c8d86fc804bb12f6fdf2a6b0478ca37ec3
Richard Chen <richard.c.chen@gmail.com>
2018-06-25 12:54:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ var katzDeli = [];
 function takeANumber(katzDeliLine, newname){
   console.log(katzDeliLine);
   katzDeliLine.push(newname);
-  return "Welcome, " + newname + ". You are number " + [katzDeliLine.length + 1] + " in line.";
+  return "Welcome, " + newname + ". You are number " + [katzDeliLine.length + 2] + " in line.";
 }
