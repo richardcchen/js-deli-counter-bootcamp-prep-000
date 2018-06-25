c99985c02c3fcaf940f97151b5be4f799dbc253a
Richard Chen <richard.c.chen@gmail.com>
2018-06-25 12:41:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var katzDeli = [];
 
 function takeANumber(katzDeliLine, newname){
   katzDeli[katzDeliLine.length + 1] = newname;
-  return [katzDeliLine.length + 1];
+  return [katzDeliLine.length + 2];
 }
