d578e0994dfccc324a6eb22d5c7b81edca3feb2f
Richard Chen <richard.c.chen@gmail.com>
2018-06-25 12:44:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var katzDeli = [];
 
 function takeANumber(katzDeliLine, newname){
   katzDeli[katzDeliLine.length + 1] = newname;
-  return "Welcome, " + newname + ". You are number " + [katzDeliLine.length] + "in line.";
+  return "Welcome, " + newname + ". You are number " + [katzDeliLine.length + 1] + " in line.";
 }
